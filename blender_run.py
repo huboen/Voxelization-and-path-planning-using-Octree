9c2d34c1209549b231db49612406190a20ea0e1d
huboen <765009202@qq.com>
2024-04-06 21:14:07 +0200

    delete files

--- blender_run.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-blender_path = "C:\\Program Files\\Blender Foundation\\blender.exe"
-blender_script_path = "B:\\Master arbeit\\blender_script.py"
-command = [blender_path,"--background","--python",blender_script_path]
-subprocess.run(command)
